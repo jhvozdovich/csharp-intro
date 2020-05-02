ea33fdb10a1e3f94e7de6ae79d7228fde51b0755
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-02 15:21:10 -0700

    refactor grocery loop to for instead of for each loop

--- GroceryList/GroceryList.cs
+++ GroceryList/GroceryList.cs
@@ -6,14 +6,16 @@ class GroceryList
     string[] myList = {"eggs", "yogurt", "granola", "cheese", "snacks", "pears"};
     Console.WriteLine("Grocery list:");
     int[] groceryPrices = {4, 3, 6, 5, 10, 6};
-    foreach (string item in myList)
+    // foreach (string item in myList)
+    for (int i= 0; i < myList.Length; i++)
     {
-      Console.WriteLine(item);
+      Console.WriteLine(myList[i]);
     }
     int total = 0;
-    foreach (int price in groceryPrices)
+    // foreach (int price in groceryPrices)
+    for (int i=0; i < groceryPrices.Length; i++)
     {
-      total += price;
+      total += groceryPrices[i];
     }
     Console.WriteLine("Your total is: $" + total);
   }
